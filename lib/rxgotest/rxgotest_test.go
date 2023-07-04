38b1096c4add798889dbbb3837d856936426fbb6
yhattm <yhattm@gmail.com>
2023-07-04 23:24:56 +0000

    refine

--- lib/rxgotest/rxgotest_test.go
+++ lib/rxgotest/rxgotest_test.go
@@ -1,8 +1,12 @@
 package rxgotest
 
 import (
+	"context"
+	"log"
 	"testing"
 	"time"
+
+	"github.com/reactivex/rxgo/v2"
 )
 
 func Test_just(t *testing.T) {
@@ -16,6 +20,22 @@ func Test_Interval(t *testing.T) {
 	OnNext(obs)
 	DoOnCompleted(obs)
 	Observe(obs.Take(5))
+	time.Sleep(time.Second * 5)
+}
+
+func Test_OnNext(t *testing.T) {
+	obs := NewObsFromInterval()
+	ctx, cancel := context.WithCancel(context.Background())
+	obs.DoOnNext(
+		func(i interface{}) {
+			log.Println("OnNext", i)
+		},
+		rxgo.WithContext(ctx),
+	)
+	OnNext(obs)
+	DoOnCompleted(obs)
+	time.Sleep(time.Second * 5)
+	cancel()
 	time.Sleep(time.Second * 5)
 }
 
