c694f780f0a0f3c45cc97eae60ba534cbbe6fda4
yhattm <yhattm@gmail.com>
2023-07-04 22:57:39 +0000

    add interval

--- lib/rxgotest/rxgotest.go
+++ lib/rxgotest/rxgotest.go
@@ -2,16 +2,32 @@ package rxgotest
 
 import (
 	"log"
+	"time"
 
 	"github.com/reactivex/rxgo/v2"
 )
 
-func just() { 
-	obs := rxgo.Just(1, 2, 3)()
+func NewObsFromJust() rxgo.Observable {
+	return rxgo.Just(1, 2, 3)()
+}
+
+func NewObsFromInterval() rxgo.Observable {
+	return rxgo.Interval(rxgo.WithDuration(time.Second))
+}
+
+func Observe(obs rxgo.Observable) {
 	for item := range obs.Observe() {
 		log.Println(item)
 		if item.E != nil {
 			panic(item.E)
 		}
 	}
+}
+
+func OnNext(obs rxgo.Observable) {
+	obs.DoOnNext(
+		func(i interface{}) {
+			log.Println("OnNext", i)
+		})
+
 }
--- lib/rxgotest/rxgotest_test.go
+++ lib/rxgotest/rxgotest_test.go
@@ -1,7 +1,19 @@
 package rxgotest
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func Test_just(t *testing.T) {
-	just()
+	obs := NewObsFromJust()
+	OnNext(obs)
+	Observe(obs)
+}
+
+func Test_Interval(t *testing.T) {
+	obs := NewObsFromInterval()
+	OnNext(obs)
+	Observe(obs.Take(5))
+	time.Sleep(time.Second * 5)
 }
